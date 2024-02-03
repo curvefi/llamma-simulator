7a3e9383bea95d890846d1f154fd041330923e1d
Michael Egorov <michwill@gmail.com>
2024-02-03 21:34:47 +0100

    CRV update, ARB sims

--- example_arb/1_simulate_fee.py
+++ example_arb/1_simulate_fee.py
@@ -4,11 +4,11 @@ from libsimulate import scan_param, plot_losses
 
 if __name__ == '__main__':
     results = scan_param(
-        'data/crvusdt-1m.json.gz',
-        A=[int(a) for a in logspace(log10(10), log10(300), 20)],
+        'data/arbusdt-1m.json.gz',
+        A=50,
         range_size=4,
-        fee=0.006,
+        fee=logspace(log10(0.0005), log10(0.05), 20),
         Texp=600
     )
 
-    plot_losses('A', results)
+    plot_losses('fee', results)
---
+++ example_arb/2_simulate_A.py
@@ -0,0 +1,14 @@
+from numpy import logspace, log10
+from libsimulate import scan_param, plot_losses
+
+
+if __name__ == '__main__':
+    results = scan_param(
+        'data/arbusdt-1m.json.gz',
+        A=[int(a) for a in logspace(log10(10), log10(1000), 20)],
+        range_size=4,
+        fee=0.007,
+        Texp=600
+    )
+
+    plot_losses('A', results)

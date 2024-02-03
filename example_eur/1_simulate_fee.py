f7ca03e1eebd7312c97437f4c055e3d17fc45a53
Michael Egorov <michwill@gmail.com>
2024-02-03 13:48:44 +0100

    EUR simulations

---
+++ example_eur/1_simulate_fee.py
@@ -0,0 +1,15 @@
+from numpy import logspace, log10
+from libsimulate import scan_param, plot_losses
+
+
+if __name__ == '__main__':
+    results = scan_param(
+        'data/eurusdt-1m.json.gz',
+        A=100,
+        range_size=4,
+        fee=logspace(log10(0.0002), log10(0.01), 20),
+        Texp=600,
+        n_top_samples=150
+    )
+
+    plot_losses('fee', results)

839f7fe051584bc16b236fd40c845ceca696699b
Michael Egorov <michwill@gmail.com>
2024-02-05 12:58:06 +0100

    TRX simulations

---
+++ example_trx/1_simulate_fee.py
@@ -0,0 +1,14 @@
+from numpy import logspace, log10
+from libsimulate import scan_param, plot_losses
+
+
+if __name__ == '__main__':
+    results = scan_param(
+        'data/trxusdt-1m.json.gz',
+        A=50,
+        range_size=4,
+        fee=logspace(log10(0.0005), log10(0.05), 20),
+        Texp=600
+    )
+
+    plot_losses('fee', results)

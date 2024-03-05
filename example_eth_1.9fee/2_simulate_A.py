fee8d6813b3348e1285b101eb86efbd205f4be48
Michael Egorov <michwill@gmail.com>
2024-03-05 20:27:16 +1100

    Add simulations with fee=1.9% for ETH

---
+++ example_eth_1.9fee/2_simulate_A.py
@@ -0,0 +1,16 @@
+from numpy import logspace, log10
+from libsimulate import scan_param, plot_losses
+
+
+if __name__ == '__main__':
+    results = scan_param(
+        'data/ethusdt-1m.json.gz',
+        A=[int(a) for a in logspace(log10(30), log10(500), 20)],
+        range_size=4,
+        fee=0.019,
+        Texp=600,
+        add_reverse=False,
+        min_loan_duration=0.05, max_loan_duration=0.05
+    )
+
+    plot_losses('A', results)
---
+++ example_eth_1.9fee/3_simulate_avg_loss.py
@@ -0,0 +1,17 @@
+from numpy import linspace
+from libsimulate import scan_param, plot_losses
+
+
+if __name__ == '__main__':
+    results = scan_param(
+        'data/ethusdt-1m.json.gz',
+        A=100,
+        range_size=[int(x) for x in linspace(4, 50, 15)],
+        fee=0.019,
+        Texp=600,
+        samples=20000, n_top_samples=20000,
+        add_reverse=False,
+        min_loan_duration=1, max_loan_duration=1
+    )
+
+    plot_losses('N', [results[0]])

0d1106d7b3d7a1d9933ad07de084ce18e492a549
Michael Egorov <michwill@gmail.com>
2024-03-30 17:48:18 +0100

    Simulations for ARB token

--- example_arb/2_simulate_A.py
+++ example_arb/2_simulate_A.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
         'data/arbusdt-1m.json.gz',
         A=[int(a) for a in logspace(log10(10), log10(1000), 20)],
         range_size=4,
-        fee=0.007,
+        fee=0.0045,
         Texp=600
     )
 

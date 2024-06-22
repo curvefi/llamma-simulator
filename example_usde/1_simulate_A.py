585735f5fc078b0e33412260fb75e5ba76bf8e3d
Michael Egorov <michwill@gmail.com>
2024-06-22 15:57:11 +0200

    Ethena simulations

---
+++ example_usde/1_simulate_A.py
@@ -0,0 +1,16 @@
+from numpy import logspace, log10
+from libsimulate import scan_param, plot_losses
+
+
+if __name__ == '__main__':
+    results = scan_param(
+        'example_usde/usdeusdc-1m.json.gz',
+        A=sorted(set(int(a) for a in logspace(log10(10), log10(2000), 50))),
+        range_size=4,
+        fee=0.002,
+        Texp=600,
+        add_reverse=True,
+        min_loan_duration=0.5, max_loan_duration=0.5
+    )
+
+    plot_losses('A', results)

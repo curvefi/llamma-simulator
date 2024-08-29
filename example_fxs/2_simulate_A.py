e710d84db08a52d7275986dc11ba601b2a13c9e2
Michael Egorov <michwill@gmail.com>
2024-08-29 10:38:32 +0200

    FXS resim

--- example_fxs/2_simulate_A.py
+++ example_fxs/2_simulate_A.py
@@ -4,14 +4,16 @@ from libsimulate import scan_param, plot_losses
 
 if __name__ == '__main__':
     results = scan_param(
-        'data/fxsusdt-1m.json.gz',
-        A=sorted(set(int(a) for a in logspace(log10(3), log10(200), 50))),
+        'example_fxs/fxsusdt-1m.json.gz',
+        A=sorted(set(int(a) for a in logspace(log10(10), log10(100), 40))),
         range_size=4,
-        fee=0.003,
-        min_loan_duration=1/24/2, max_loan_duration=1/24/2,
+        fee=0.015,  # 3 x feed fee
+        Texp=600,
         add_reverse=True,
-        samples=2000000,
-        Texp=600
+        min_loan_duration=1/24/2, max_loan_duration=1/24/2,
+        n_top_samples=1,  # Uses the very top loss of all
+        samples=5_000_000,
+        other={'dynamic_fee_multiplier': 0.5, 'use_po_fee': 1, 'po_fee_delay': 2}
     )
 
     plot_losses('A', results)

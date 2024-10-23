05f51fe36c30ae3eee22bd3fd0c6c5c8bb27be64
Michael Egorov <michwill@gmail.com>
2024-10-23 22:38:18 +0200

    Gold simulations / strange results

---
+++ example_gold/2_simulate_A.py
@@ -0,0 +1,16 @@
+from numpy import logspace, log10
+from libsimulate import scan_param, plot_losses
+
+
+if __name__ == '__main__':
+    results = scan_param(
+        'example_gold/xauusd-1m.json.gz',
+        A=sorted(set([int(a) for a in logspace(log10(3), log10(300), 20)])),
+        range_size=4,
+        fee=0.004,
+        Texp=600,
+        samples=300_000,
+        n_top_samples=3
+    )
+
+    plot_losses('A', results)
